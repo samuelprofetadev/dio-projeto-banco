ca3bc7797cb798354628e4101852bd73018acd3f
samuel <saamuka70@gmail.com>
2024-01-24 19:26:46 -0300

    aa

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Digite o número da Agência: ");
+        String agencia = sc.next();
+
+        System.out.println("Digite seu nome: ");
+        String nome = sc.next();
+
+        System.out.println("Digite seu saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println(numero);
+        System.out.println(nome);
+        System.out.println(saldo);
+        System.out.println(agencia);
+
+
+
 
     }
 }
